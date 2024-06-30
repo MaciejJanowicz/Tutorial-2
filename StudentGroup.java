1d537e312cc36f6009ef59ea1036e6b72a66da4c
MaciejJanowicz <163749512+MaciejJanowicz@users.noreply.github.com>
2024-06-30 09:43:32 +0200

    StudentGroup.java

--- StudentGroup.java
+++ StudentGroup.java
@@ -1,5 +1,25 @@
-public class StudentGroup {
+import java.util.ArrayList;
+import java.util.List;
 
-        public String fname;
+public class StudentGroup {
+    public String name;
+    public List<Student> students;
+
+    private static final int MAX_GROUP_SIZE = 15;
+
+    StudentGroup() {
+        students = new ArrayList<>();
     }
 
+    public void addStudent(Student student) {
+        if (students.contains(student)) {
+            throw new IllegalStateException("This student exists in group.");
+        }
+
+        if (students.size() == MAX_GROUP_SIZE) {
+            throw new IllegalStateException("Group is full.");
+        }
+
+        students.add(student);
+    }
+}
